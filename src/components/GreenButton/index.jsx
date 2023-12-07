7a379b6cde1f6b3590eb102cb5c28e62612eda19
Moghadam80 <etehadian6@gmail.com>
2023-12-07 10:43:36 +0330

    complete login form

--- src/components/GreenButton/index.jsx
+++ src/components/GreenButton/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const GreenButton = ({type = 'button',onClick, className, text}) => {
+const GreenButton = ({type = 'button',Click, className, text}) => {
   return (
     <div>
-        <button type={type} className={`${className} my-6 rounded-[15px] bg-[#00C853] hover:shadow-md active:border-2 active:border-[#00712F] text-white`}>
+        <button type={type} onClick={Click} className={`${className} my-6 rounded-[15px] bg-[#00C853] hover:shadow-md active:border-2 active:border-[#00712F] text-white`}>
             {text}
         </button> 
     </div>
